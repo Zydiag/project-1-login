f4e6a8a37cd819c3975d95131b9fffcd914b79d7
Sahil <dshiv867@gmail.com>
2024-01-17 22:05:34 +0530

    created login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,27 @@
+import Logo from './assets/logo.png';
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="container">
+      <div className="leftSide">
+        <div className="logoContainer">
+          <img src={Logo} alt="logo" className="logo" />
+          <span className="title">
+            Code
+            <span className="title2">Cuisine</span>
+          </span>
+        </div>
+        <div className="inputContainer">
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="Email" />
+          <button>Login</button>
+        </div>
+        <p className="link">
+          Don&apos;t have an account? <a href="">Sign Up</a>
+        </p>
+      </div>
+      <div className="rightSide"></div>
+    </div>
+  );
 };
 
 export default Login;
